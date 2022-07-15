8afe35e619498c42c37b0169080cbf2b4648ef47
ushy76 <u.brandler@gmx.de>
2022-07-15 17:47:34 +0200

    created script.js + folder src

---
+++ src/script.js
@@ -0,0 +1,47 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let day = days[dayIndex];
+  return ` ${day}, ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = response.data.main.temp;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "3f6ee3a62ec71d781b4d82db2a46c73d";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);
+
+dateElement.innerHTML = formatDate(currentTime);
